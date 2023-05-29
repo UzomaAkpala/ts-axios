90db75c3ceb4d43e986763dafcd7747d31a3b3c5
UzomaAkpala <francisakpala@yahoo.com>
2023-05-29 13:36:40 +0100

    tpyes edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import  { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios'
-
-export type Crypto = {
-  ath: number;
-  atl: number;
-  current_price:number;
-  id: string;
-  name: string;
-  symbol: string;
-  high_24h: number;
-  low_24h: number;
-
-}
+import CryptoSummary from './components/CryptoSummary';
+import { Crypto } from './Types';
 
 function App() {
   const [cryptos, setCrytops] = useState<Crypto[] | null>()
@@ -26,7 +16,7 @@ function App() {
   
   return <div className="App">{cryptos ? 
   cryptos.map((crypto) => {
-    return <p>{crypto.name + ' $' + crypto.current_price}</p>
+   return  <CryptoSummary crypto = {crypto} />
   }) : null}</div>;
 }
 
